525aa176771dce5451d4c77e842f25c561230111
Andrea <AndryGabry01@gmail.com>
2021-11-21 14:52:58 +0100

    Add files via upload

---
+++ StarPacMantGame.bat
@@ -0,0 +1,11 @@
+@echo off
+pip install -r requirements.txt >> nul && (
+  Echo "Install/Update requirements"
+) || (
+    Echo "failed requirements Installd/Updated or execute pip command... I still try to start the game"
+)
+python "PacManGui.py" >> nul && (
+  Echo "StartGame"
+) || (
+    Echo "failed start Game"
+)
